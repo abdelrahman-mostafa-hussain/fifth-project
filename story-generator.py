c3886d02549de7fc84985f4c714098177fef7f7b
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-29 16:01:15 +0200

    generate random template and read it

--- story-generator.py
+++ story-generator.py
@@ -1,3 +1,12 @@
+import random
+
+def randomNumTemplate():
+    return random.choice(range(1,5))
+
+def openAndReadStoryFile():
+    file = open(f"template-{randomNumTemplate}.txt")
+    return file.read()
+
 def main():
     print("this is story generator")
 
