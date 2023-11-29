b6e2fa863fa765a4c57b7096d6412bac506f5039
abdelrahman <eng.abdelrahman.mostafa.hussain@gmail.com>
2023-11-29 16:27:10 +0200

    the story variable and print story

--- story-generator.py
+++ story-generator.py
@@ -9,11 +9,21 @@ def openAndReadStoryFile():
 
 def main():
     content=openAndReadStoryFile()
-    noun=input("enter the noun")
-    name=input("enter the name")
-    verb=input("enter the verb")
-    adjective=input("enter the adjective")
+    noun=input("enter the noun: ")
+    name=input("enter the name: ")
+    verb=input("enter the verb: ")
+    adjective=input("enter the adjective: ")
 
+    if noun!=None:
+        content=content.replace("[noun]",f"{noun}")
+    if name!=None:
+        content=content.replace("[name]",f"{name}")
+    if verb!=None:
+        content=content.replace("[verb]",f"{verb}")
+    if adjective!=None:
+        content=content.replace("[adjective]",f"{adjective}") 
+
+    print(content)   
 
 if __name__=="__main__":
     main()
